35f906babdbfcfcee6e15a8808563f55ffe1a6b5
Gaspard Bucher <gaspard@teti.ch>
2006-12-22 20:27:05 +0000

    r215@alu:  gaspard | 2006-12-22 21:19:51 +0100  Tested comments and fixed menu bug
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@118 aaa19a3b-e121-0410-9d45-962fd44e537e

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,27 +1,31 @@
 =begin
-Comments can be added on a per discussion basis. They can be replies to other comments in the same
-discussion. They are signed by the user commenting. Public comments
-belong to the user _anon_ (see #User).
+Comments can be added on a per discussion basis. There can be replies to other comments in the same
+discussion. Comments are signed by the user commenting. Public comments
+belong to the user _anon_ (see #User) and must have the 'athor_name' field set.
+
+If ZENA_ENV[:moderate_public_comments] is set, all public comments are set to 'prop' and are not directly seen on the site.
 =end
 class Comment < ActiveRecord::Base
   belongs_to :author, :class_name=>'User', :foreign_key=>'user_id'
   belongs_to :discussion
   has_many   :replies, :class_name=>'Comment', :foreign_key=>'reply_to', :order=>'created_at ASC', :conditions=>"status = '#{Zena::Status[:pub]}'"
-  belongs_to :parent, :class_name=>'Comment' , :foreign_key=>'reply_to'
+  belongs_to :parent,  :class_name=>'Comment', :foreign_key=>'reply_to'
   validates_presence_of :text
   validates_presence_of :title
-  before_validation :set_comment
-  validate :valid_comment
+  validates_presence_of :discussion
+  validates_presence_of :author_name, :if => Proc.new {|obj| obj.user_id == 1 }
+  before_validation_on_create :set_comment
   
-  # TODO: test all above and below !!
-  
-  # TODO: test
+  # Remove the comment (set it's status to +rem+)
   def remove
-    update_attributes( :status=> Zena::Status[:rem])
+    self.class.logger.info 'REMOVE ====== ['
+    update_attributes( :status=> Zena::Status[:rem] )
+    self.class.logger.info '] ====== REMOVE'
   end
   
   private
   def set_comment
+    return false unless discussion
     if parent && (self[:title].nil? || self[:title] == '')
       self[:title] = TransKey['re:'][discussion.lang] + ' ' + parent.title
     end
@@ -30,13 +34,8 @@ class Comment < ActiveRecord::Base
     else
       self[:status] = Zena::Status[:pub]
     end
-  end
-  
-  def valid_comment
-    if user_id == 1
-      errors.add('author_name', 'cannot be blank') unless author_name && author_name != ''
-    else
-      self[:author] = nil
+    if user_id != 1
+      self[:author_name] = nil
     end
   end
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -19,5 +19,61 @@ class CommentTest < Test::Unit::TestCase
     comment = comments(:lion_says_inside)
     assert_equal 'PLV', comment.author.initials
   end
+  
+  def test_remove
+    comment = comments(:lion_says_inside)
+    assert_equal Zena::Status[:pub], comment[:status]
+    assert comment.remove
+    comment = comments(:lion_says_inside)
+    assert_equal Zena::Status[:rem], comment[:status]
+  end
+  
+  def test_moderate_public_comments
+    bak = ZENA_ENV[:moderate_public_comments]
+    discussion = Discussion.create(:item_id=>11, :lang=>'en')
+    ZENA_ENV[:moderate_public_comments] = true
+    comment = Comment.create( :user_id=>1, :title=>'coco', :text=>'spam see my web site', :author_name=>'me', :discussion_id=>discussion[:id] )
+    assert !comment.new_record?, "Not a new record"
+    assert_equal Zena::Status[:prop], comment[:status]
+    ZENA_ENV[:moderate_public_comments] = false
+    comment = Comment.create( :user_id=>1, :title=>'coco again', :text=>'spam see my web site again', :author_name=>'me', :discussion_id=>discussion[:id] )
+    assert !comment.new_record?, "Not a new record"
+    assert_equal Zena::Status[:pub], comment[:status]
+    ZENA_ENV[:moderate_public_comments] = bak
+    comments = discussion.comments
+    assert_equal 1, comments.size
+  end
+  
+  def test_set_comment
+    comment    = comments(:lion_says_inside)
+    discussion = comment.discussion
+    reply      = Comment.create( :user_id=>1, :text=>'blah blah', :author_name=>'me', :reply_to=>comment[:id], :discussion_id=>discussion[:id] )
+    assert !reply.new_record?, "Not a new record"
+    assert_equal 're: OK for me', reply[:title]
+  end
+  
+  def test_valid_comment
+    comment = Comment.create( :user_id=>1, :title=>'boo', :text=>'blah', :discussion_id=>2 )
+    assert comment.new_record?, "Is a new record"
+    assert_equal "can't be blank", comment.errors[:author_name]
     
+    comment = Comment.create( :user_id=>3, :title=>'boo', :text=>'blah', :discussion_id=>2 )
+    err comment
+    assert ! comment.new_record?, "Not a new record"
+    assert_nil comment.author_name, "Author name is nil"
+  end
+  
+  def test_replies
+    comment = comments(:tiger_reply_inside)
+    reply1 = Comment.create( :user_id=>1, :title=>'boo1', :author_name=>'bob', :text=>'blah', :discussion_id=>2, :reply_to=>comment[:id] )
+    err reply1
+    reply2 = Comment.create( :user_id=>1, :title=>'boo2', :author_name=>'lisa', :text=>'blah', :discussion_id=>2, :reply_to=>comment[:id] )
+    replies = comment.replies
+    assert_equal 2, replies.size
+    Comment.connection.execute "UPDATE comments SET status=#{Zena::Status[:prop]} WHERE id = #{reply2[:id]}"
+    comment = comments(:tiger_reply_inside)
+    replies = comment.replies
+    assert_equal 1, replies.size # prop not seen
+    assert_equal 'boo1', replies[0][:title]
+  end
 end
