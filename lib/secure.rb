a90e97161b451c706697ca8f52aa10e305aeaa2b
Gaspard Bucher <gaspard@teti.ch>
2007-02-21 15:49:56 +0000

    Cannot make private if cannot 'visible' in parent
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@289 aaa19a3b-e121-0410-9d45-962fd44e537e

--- lib/secure.rb
+++ lib/secure.rb
@@ -512,18 +512,20 @@ Just doing the above will filter all result according to the logged in user.
             end
           when 0
             if old.can_visible?
-              if private?
-                # ok (all groups are 0)
-              elsif ref.can_visible?
+              if ref.can_visible?
                 # can change groups
-                if rgroup_id != 0 && rgroup_id != old[:rgroup_id] && !visitor_groups.include?(rgroup_id)
-                  errors.add('rgroup_id', "unknown group")
-                end
-                if wgroup_id != 0 && wgroup_id != old[:wgroup_id] && !visitor_groups.include?(wgroup_id)
-                  errors.add('wgroup_id', "unknown group") 
-                end
-                if pgroup_id != 0 && pgroup_id != old[:pgroup_id] && !visitor_groups.include?(pgroup_id)
-                  errors.add('pgroup_id', "unknown group")
+                if private?
+                  # ok (all groups are 0)
+                else
+                  if rgroup_id != 0 && rgroup_id != old[:rgroup_id] && !visitor_groups.include?(rgroup_id)
+                    errors.add('rgroup_id', "unknown group")
+                  end
+                  if wgroup_id != 0 && wgroup_id != old[:wgroup_id] && !visitor_groups.include?(wgroup_id)
+                    errors.add('wgroup_id', "unknown group") 
+                  end
+                  if pgroup_id != 0 && pgroup_id != old[:pgroup_id] && !visitor_groups.include?(pgroup_id)
+                    errors.add('pgroup_id', "unknown group")
+                  end
                 end
               else
                 # cannot change groups
