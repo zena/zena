fdaa2cce3e341a2372d420a00febde9783d422ff
Gaspard Bucher <gaspard@teti.ch>
2007-06-02 12:50:42 +0000

    [fix] migration fix
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@595 aaa19a3b-e121-0410-9d45-962fd44e537e

--- db/migrate/000_zena/020_create_participation.rb
+++ db/migrate/000_zena/020_create_participation.rb
@@ -9,7 +9,7 @@ class CreateParticipation < ActiveRecord::Migration
     values = select_all("SELECT * FROM sites_users").map do |r|
       "(#{['user_id','site_id','status','contact_id'].map {|k| quote(r[k])}.join(', ')})"
     end
-    execute "INSERT INTO participations (`user_id`,`site_id`,`status`,`contact_id`) VALUES #{values.join(', ')}"
+    execute "INSERT INTO participations (`user_id`,`site_id`,`status`,`contact_id`) VALUES #{values.join(', ')}" unless values == []
     execute "ALTER IGNORE TABLE participations ADD UNIQUE KEY(user_id,site_id)"
     drop_table :sites_users
   end
@@ -24,7 +24,7 @@ class CreateParticipation < ActiveRecord::Migration
     values = select_all("SELECT * FROM participations").map do |r|
       "(#{['user_id','site_id','status','contact_id'].map {|k| quote(r[k])}.join(', ')})"
     end
-    execute "INSERT INTO sites_users (`user_id`,`site_id`,`status`,`contact_id`) VALUES #{values.join(', ')}"
+    execute "INSERT INTO sites_users (`user_id`,`site_id`,`status`,`contact_id`) VALUES #{values.join(', ')}" unless values == []
     drop_table :participations
   end
 end
