6f5585ae0e3594667e19cdf32d3baa57e0bc61d5
Gaspard Bucher <gaspard@teti.ch>
2007-06-02 12:35:39 +0000

    [change] added svn:externals for vendor/rails
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@593 aaa19a3b-e121-0410-9d45-962fd44e537e

--- config/deploy_config_example.rb
+++ config/deploy_config_example.rb
@@ -1,7 +1,7 @@
-set :db_name,             "example_org" # no dots in this name !
+set :db_name,             "zena" # no dots in this name !
 set :server_ip,           "123.456.789.111"
 set :mongrel_port,        "8000"
 set :mongrel_count,       "3"
 set :db_password,         "super secret password"
-set :db_user,             "example_org"
+set :db_user,             "zena"
 set :repository,          "http://svn.zenadmin.org/zena/trunk"
