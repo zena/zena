5a02dbbeec3f262048c1b44625e5b4587745632a
Gaspard Bucher <gaspard@teti.ch>
2007-06-11 19:59:00 +0000

    [change] better 'title' and 'check_lang' zafu tags
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@626 aaa19a3b-e121-0410-9d45-962fd44e537e

--- test/helpers/zena_parser_test.rb
+++ test/helpers/zena_parser_test.rb
@@ -8,7 +8,7 @@ class ZenaParserTest < ZenaHelperTest
   Section # make sure we load Section links before trying relations
   
   def test_single
-    do_test('basic', 'show_title_check_lang')
+    do_test('basic', 'show_title_options')
   end
   
   def test_basic_show_bad_attr
