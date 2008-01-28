f9f934d2bbae6970fa58ec98ad73d10bf17d347c
Gaspard Bucher <gaspard@teti.ch>
2008-01-28 01:06:56 +0000

    testing commit hook
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@814 aaa19a3b-e121-0410-9d45-962fd44e537e

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -148,7 +148,6 @@ class VersionTest < ZenaTestUnit
   end
   
   def test_dynamic_attributes
-    assert false
     login(:tiger)
     node = secure(Node) { nodes(:status) }
     version = node.send(:redaction)
