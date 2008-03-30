6ada297a0b55a0a0a3a3991a692f04870147b633
Gaspard Bucher <gaspard@teti.ch>
2008-03-30 12:33:59 +0000

    Fixed some bugs after changes to the way fullpath is calculated
    git-svn-id: svn://svn.zenadmin.org/zena/trunk@911 aaa19a3b-e121-0410-9d45-962fd44e537e

--- app/models/node.rb
+++ app/models/node.rb
@@ -1142,11 +1142,16 @@ class Node < ActiveRecord::Base
   private
     def node_before_validation
       
-      # remove cached fullpath
-      if new_record? || self[:name] != old[:name] || self[:parent_id] != old[:parent_id]
-        self[:fullpath] = self.fullpath(true,false)
-      elsif !new_record? && self[:custom_base] != old[:custom_base]
-        self[:basepath] = self.basepath(true,false)
+      # set name from version title if name not set yet
+      self.name = version[:title] unless self[:name]
+      
+      if self[:name]
+        # update cached fullpath
+        if new_record? || self[:name] != old[:name] || self[:parent_id] != old[:parent_id]
+          self[:fullpath] = self.fullpath(true,false)
+        elsif !new_record? && self[:custom_base] != old[:custom_base]
+          self[:basepath] = self.basepath(true,false)
+        end
       end
 
       # make sure section is the same as the parent
