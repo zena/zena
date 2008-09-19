0c94ade5a67e3b664de5e2bd2f981e0f322f429f
Gaspard Bucher <gaspard@teti.ch>
2008-09-19 14:40:22 +0200

    Removed traces from old migrations (was inserting some default relations) where it should leave the relations table empty.

--- db/migrate/000_zena/021_create_relations.rb
+++ db/migrate/000_zena/021_create_relations.rb
@@ -14,27 +14,6 @@ class CreateRelations < ActiveRecord::Migration
       t.column :site_id, :integer, :null => false
     end
     add_column :links, 'relation_id', :integer
-    # load relations fixtures
-    source_rel = {}
-    target_rel = {}
-    relations  = []
-    YAML::load_documents( File.open( File.join(RAILS_ROOT, 'test', 'fixtures', 'relations.yml') ) ) do |entries|
-      entries.each do |key,value|
-        relation = []
-        ['source_role', 'source_kpath', 'source_icon', 'target_role', 'target_kpath', 'target_icon'].each do |sym|
-          relation << quote(value[sym])
-        end
-        relations << relation
-      end
-    end
-    # create relations for each site
-    Site.find(:all).each do |site|
-      insert = relations.map do |rel|
-        "(#{(rel + [site[:id]]).join(',')})"
-      end
-      execute "INSERT INTO relations (`source_role`,`source_kpath`,`source_icon`,`target_role`,`target_kpath`,`target_icon`,`site_id`) VALUES #{insert.join(', ')}"
-      execute "UPDATE links,nodes SET relation_id = (SELECT id FROM relations WHERE source_role = links.role OR target_role = links.role AND site_id = #{site[:id]} LIMIT 1) WHERE links.source_id = nodes.id AND nodes.site_id = #{site[:id]}"
-    end
   end
 
   def self.down
