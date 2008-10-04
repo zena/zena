863fcaa2b1d1f66e2e8279d1ee261393d930c6fb
Gaspard Bucher <gaspard@teti.ch>
2008-10-04 19:05:39 +0200

    First beta release. Tagged v0.10.0.

--- config/version.rb
+++ config/version.rb
@@ -1,9 +1,9 @@
 module Zena #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 9
+    MINOR = 10
     TINY  = 0
-    REV   = 1001
+    REV   = 1214
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
 end
