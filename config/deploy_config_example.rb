64ae1b47e5ad97096c77b1fffc3cf6bfe61a5101
Gaspard Bucher <gaspard@teti.ch>
2008-11-21 17:39:16 +0100

    Fixed install instructions after testing on a clean debian box.

--- config/deploy_config_example.rb
+++ config/deploy_config_example.rb
@@ -1,7 +1,7 @@
 set :db_name,             "zena" # no dots in this name !
-set :server_ip,           "123.456.789.111"
+set :server_ip,           "127.0.0.1"
 set :mongrel_port,        "8000"
 set :mongrel_count,       "3"
-set :db_password,         "super secret password"
+set :db_password,         "secret"
 set :db_user,             "zena"
 set :repository,          "http://svn.zenadmin.org/zena/trunk"
