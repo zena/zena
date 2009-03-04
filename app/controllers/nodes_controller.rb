0c1960ab06f374cb75ff0bf1b5c9a2c9b202d0c5
Gaspard Bucher <gaspard@teti.ch>
2009-03-04 16:45:07 +0100

    Started implementation to use X-Sendfile response hearders to send documents faster.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -186,32 +186,25 @@ class NodesController < ApplicationController
           # math rendered as png, ...
           filename     = "#{asset}.#{params[:format]}"
           content_path = @node.asset_path(filename)
-          raise ActiveRecord::RecordNotFound unless File.exist?(content_path)
-          data = File.new(content_path)
           content_type = (EXT_TO_TYPE[params[:format]] || ['application/octet-stream'])[0]
-          send_data( data.read , :filename=>filename, :type => content_type, :disposition=>'inline')
-          data.close
+          send_file(:path => content_path, :filename=>filename, :type => content_type, :disposition=>'inline')
           cache_page(:content_path => content_path, :authenticated => @node.public?) # content_path is used to cache by creating a symlink
         elsif @node.kind_of?(Document) && params[:format] == @node.c_ext
           # Get document data (inline if possible)
-        
+          
           if @node.kind_of?(Image) && !ImageBuilder.dummy?
             if img_format = Iformat[params[:mode]]
-              data = @node.c_file(img_format)
               content_path = @node.c_filepath(img_format)
+              # force creation of image data
+              @node.c_file(img_format)
             end
           elsif @node.kind_of?(TextDocument)
             data = StringIO.new(@node.v_text)
-            content_path = nil
           else
-            data         = @node.c_file
             content_path = @node.c_filepath
           end
           
-          raise ActiveRecord::RecordNotFound unless data
-        
-          send_data( data.read , :filename=>@node.filename, :type => @node.c_content_type, :disposition=>'inline')
-          data.close
+          send_file( :path => content_path, :data => data, :filename=>@node.filename, :type => @node.c_content_type, :disposition=>'inline')
           cache_page(:content_path => content_path, :authenticated => @node.public?) # content_path is used to cache by creating a symlink
         else
           render_and_cache
@@ -284,9 +277,7 @@ class NodesController < ApplicationController
   end
   
   def export
-    data = @node.archive
-    send_data( data.read, :filename=>"#{@node.name}.tgz", :type => 'application/x-gzip')
-    data.close
+    send_file( :file => @node.archive, :filename=>"#{@node.name}.tgz", :type => 'application/x-gzip')
   end
   
   def update
@@ -389,6 +380,29 @@ class NodesController < ApplicationController
   
   
   protected
+    def send_file(opts)
+      content_path = nil
+      if data = opts.delete(:data)
+        send_data(data.read, opts)
+        return
+      elsif file = opts.delete(:file)
+        content_path = file.path
+      else
+        content_path = opts.delete(:path)
+      end
+      
+      raise ActiveRecord::RecordNotFound if content_path.nil? || !File.exist?(content_path)
+      
+      if ENABLE_XSENDFILE
+        response.headers['Content-Type'] = opts[:type] if opts[:type]
+        response.headers['Content-Disposition'] = "inline; filename=\"#{opts[:filename]}\"" if opts[:filename]
+        response.headers["X-Sendfile"] = content_path
+        response.headers['Content-length'] = File.size(content_path)
+        render :nothing => true
+      else
+        send_data(File.read(content_path), opts)
+      end
+    end
     
     # Find a node based on the path or id. When there is a path, the node is found using the zip included in the path
     # or by fullpath:
