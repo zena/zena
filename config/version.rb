a30c637a68827181c10b70a630c59c4aa346404e
Gaspard Bucher <gaspard@teti.ch>
2009-04-05 23:51:14 +0200

    More tests for custom queries using latest querybuilder (0.5.2).

--- config/version.rb
+++ config/version.rb
@@ -2,7 +2,7 @@ module Zena #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 11
-    TINY  = 0
+    TINY  = 1
     REV   = File.exist?(File.join(RAILS_ROOT, 'REVISION')) ? File.read(File.join(RAILS_ROOT, 'REVISION'))[0..5].to_i.to_s : '1280+'
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
