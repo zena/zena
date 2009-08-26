221915bd7a5783df0def2988a25cceb1c36de804
Gaspard Bucher <gaspard@teti.ch>
2009-08-26 12:46:32 +0200

    Easier to add extension synonyms to declared mime types. Added 'jpg' for testing.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,10 @@
-['bin'].each do |ext|
-  Mime::Type.register Rack::Mime::MIME_TYPES[".#{ext}"], ext.to_sym
+[
+  'bin',
+  ['jpg', 'jpeg'],
+].each do |ext|
+  if ext.kind_of?(Array)
+    Mime::Type.register Rack::Mime::MIME_TYPES[".#{ext}"], ext.first.to_sym, [], ext[1..-1]
+  else
+    Mime::Type.register Rack::Mime::MIME_TYPES[".#{ext}"], ext.to_sym
+  end
 end
