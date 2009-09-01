72fe1acbf3c0ad359af80807d3bd97da0c6cc9d6
Gaspard Bucher <gaspard@teti.ch>
2009-09-01 17:48:15 +0200

    Moved test into it's own file corresponding to module.

---
+++ test/unit/zena/use/zafu_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ZafuTest < Zena::Unit::TestCase
+  include Zena::Use::Zafu::ViewMethods
+  include Zena::Use::Zafu::ControllerMethods
+  
+  def test_page_numbers
+    s = ""
+    page_numbers(2, 3, ',') {|p,j| s << "#{j}#{p}"}
+    assert_equal "1,2,3", s
+    s = ""
+    page_numbers(2, 30, ',') {|p,j| s << "#{j}#{p}"}
+    assert_equal "1,2,3,4,5,6,7,8,9,10", s
+    s = ""
+    page_numbers(14, 30, ',') {|p,j| s << "#{j}#{p}"}
+    assert_equal "10,11,12,13,14,15,16,17,18,19", s
+    s = ""
+    page_numbers(28, 30, ' | ') {|p,j| s << "#{j}#{p}"}
+    assert_equal "21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30", s
+  end
+end
