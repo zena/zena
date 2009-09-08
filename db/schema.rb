55d349a83a77a2311706aa9126b9290535082ace
Gaspard Bucher <gaspard@teti.ch>
2009-09-08 13:25:52 +0200

    Merged conflict with migration number from EXIF feature in stable branch.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 52) do
+ActiveRecord::Schema.define(:version => 20090825201200) do
 
   create_table "cached_pages", :force => true do |t|
     t.text     "path"
