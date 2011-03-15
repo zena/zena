0e023e838db13d3180ca9b0647fa5200eca86d3e
Gaspard Bucher <gaspard@teti.ch>
2011-03-15 16:49:56 +0100

    More work on data migration from 0.x to 1.0 (tested).

--- bricks/zena/zena/migrate/03_zerox1_data.rb
+++ bricks/zena/zena/migrate/03_zerox1_data.rb
@@ -1,4 +1,4 @@
-
+require 'fileutils'
 # ============================================ contact_contents
 # migrate content to properties
 class ContactContent < ActiveRecord::Base
@@ -53,7 +53,7 @@ class DocumentContent < ActiveRecord::Base
       fname = name.gsub(/[^a-zA-Z\-_0-9]/,'')
 
       if File.exist?("#{SITES_ROOT}#{$site.data_path}/full") && !File.exist?("#{SITES_ROOT}#{$site.data_path}/full_old")
-        File.mv("#{SITES_ROOT}#{$site.data_path}/full","#{SITES_ROOT}#{$site.data_path}/full_old")
+        FileUtils.mv("#{SITES_ROOT}#{$site.data_path}/full","#{SITES_ROOT}#{$site.data_path}/full_old")
       end
       old_path = "#{SITES_ROOT}#{$site.data_path}/full_old/#{digest[0..0]}/#{digest[1..1]}/#{digest[2..2]}/#{fname}"
 
@@ -99,6 +99,15 @@ class DynAttribute < ActiveRecord::Base
   end
 end
 
+# ============================================ site_attributes
+class SiteAttribute < ActiveRecord::Base
+  set_table_name :site_attributes
+
+  def migrate(site)
+    site.prop[key] = value
+  end
+end
+
 # ============================================ template_contents
 # migrate content to properties
 # rebuild index ==> should recreate idx_templates content
@@ -121,13 +130,23 @@ end
 class VersionMig < ActiveRecord::Base
   set_table_name :versions
   belongs_to :node, :class_name => 'NodeMig', :foreign_key => 'node_id'
-  has_one :contact_content, :foreign_key => 'version_id'
-  has_one :document_content, :foreign_key => 'version_id'
   has_one :template_content, :foreign_key => 'node_id', :primary_key => 'node_id'
 
   include Property
   # Should be the same serialization as in Version and Site
   include Property::Serialization::JSON
+
+  def contact_content
+    @contact_content ||= ContactContent.find(:first,
+      :conditions => ['version_id = ?', content_id || id]
+    )
+  end
+
+  def document_content
+    @document_content ||= DocumentContent.find(:first,
+      :conditions => ['version_id = ?', content_id || id]
+    )
+  end
 
   def migrate!
     migrate_base
@@ -196,11 +215,105 @@ end
 # === dyn_attributes
 
 class SiteMig < ActiveRecord::Base
+  CHUNK_SIZE = 200
+  has_many :site_attributes, :class_name => 'SiteAttribute', :foreign_key => 'owner_id'
+
+  include Property
+
   set_table_name :sites
 
   def data_path
     "/#{self[:host]}/data"
   end
+
+  def migrate!
+    # needed by DocumentContent
+    $site = self
+
+    # Allow any property
+    class << self.prop
+      def validate
+        # do nothing
+        true
+      end
+    end
+
+    migrate_nodes
+    migrate_site
+    if !save
+      puts "Could not save site #{id} (#{host}):"
+      errors.each_error do |er,msg|
+        puts "  [#{er}] #{msg}"
+      end
+    end
+  end
+
+  def migrate_nodes
+    node_count = NodeMig.count(:conditions =>['site_id = ?', id])
+    puts "========================================== Start data migration to 1.0 for #{host} (#{node_count} nodes)"
+    puts "========== creating virtual classes (Contact, Reference)"
+    # Create Contact + Reference virtual classes
+    ref_class = VClassMig.create(
+      :name       => 'Reference',
+      :kpath      => 'NR',
+      :real_class => 'Node',
+      :create_group_id => public_group_id,
+      :site_id    => id
+    )
+    contact_class = VClassMig.create(
+      :name       => 'Contact',
+      :kpath      => 'NRC',
+      :real_class => 'Node',
+      :create_group_id => public_group_id,
+      :site_id    => id
+    )
+    # Update nodes depending on Contact + Reference classes
+    execute "UPDATE nodes SET vclass_id = #{ref_class.id} WHERE type = 'Reference' AND vclass_id IS NULL AND site_id = #{id}"
+    execute "UPDATE nodes SET vclass_id = #{contact_class.id} WHERE type = 'Contact' AND vclass_id IS NULL AND site_id = #{id}"
+    execute "UPDATE nodes SET type = 'Node' WHERE (type = 'Reference' or type = 'Contact') AND site_id = #{id}"
+
+    page  = 0
+    while true do
+      nodes = NodeMig.find(:all,
+        :conditions => ['site_id = ?', id],
+        :limit  => CHUNK_SIZE,
+        :offset => page * CHUNK_SIZE,
+        :order  => 'id ASC'
+      )
+      break if nodes == []
+      puts "========== migrating nodes #{(page * CHUNK_SIZE) + 1} to #{[((page+1) * CHUNK_SIZE), node_count].min}"
+      nodes.each do |n|
+        n.versions.each do |v|
+          v.migrate!
+        end
+      end
+      page += 1
+    end
+
+    # ============================================ nodes
+    # 1. Set skin_id from skin name
+    NodeMig.find(:all,
+      :conditions => ['site_id = ? AND type = ?', id, 'Skin']
+    ).each do |skin|
+      execute "UPDATE nodes SET skin_id = #{skin.id} WHERE skin = '#{skin.name}'"
+    end
+
+    # 2. Set _id from name
+    execute "UPDATE nodes SET _id = name WHERE site_id = #{id}"
+  end
+
+  def migrate_site
+    # ============================================ site_attributes
+    # migrate to properties
+    site_attributes.each do |attribute|
+      attribute.migrate(self)
+    end
+  end
+
+  private
+    def execute(*args)
+      self.class.connection.execute(*args)
+    end
 end
 
 
@@ -208,8 +321,6 @@ end
 # to Zerox1Schema.
 class Zerox1Data < ActiveRecord::Migration
   extend Zena::Acts::Secure
-
-  CHUNK_SIZE = 200
   def self.up
     if connection.tables.include?('contact_contents')
       # Need to migrate data
@@ -229,70 +340,16 @@ class Zerox1Data < ActiveRecord::Migration
 
     execute "UPDATE roles SET real_class = 'Node' WHERE real_class = 'Reference' OR real_class = 'Contact'"
 
-    # sites = SiteMig.all
-    sites = [SiteMig.find(:first, :conditions => ['host = ?', 'lubyk.org'])]
+    sites = SiteMig.all
     sites.each do |site|
-      node_count = NodeMig.count(:conditions =>['site_id = ?', site.id])
-      puts "========================================== Start data migration to 1.0 for #{site.host} (#{node_count} nodes)"
-      puts "========== creating virtual classes (Contact, Reference)"
-      # Create Contact + Reference virtual classes
-      ref_class = VClassMig.create(
-        :name       => 'Reference',
-        :kpath      => 'NR',
-        :real_class => 'Node',
-        :create_group_id => site.public_group_id,
-        :site_id    => site.id
-      )
-      contact_class = VClassMig.create(
-        :name       => 'Contact',
-        :kpath      => 'NRC',
-        :real_class => 'Node',
-        :create_group_id => site.public_group_id,
-        :site_id    => site.id
-      )
-      # Update nodes depending on Contact + Reference classes
-      execute "UPDATE nodes SET vclass_id = #{ref_class.id} WHERE type = 'Reference' AND vclass_id IS NULL AND site_id = #{site.id}"
-      execute "UPDATE nodes SET vclass_id = #{contact_class.id} WHERE type = 'Contact' AND vclass_id IS NULL AND site_id = #{site.id}"
-      execute "UPDATE nodes SET type = 'Node' WHERE (type = 'Reference' or type = 'Contact') AND site_id = #{site.id}"
-
-      $site = site
-      page  = 0
-      while true do
-        nodes = NodeMig.find(:all,
-          :conditions => ['site_id = ?', site.id],
-          :limit  => CHUNK_SIZE,
-          :offset => page * CHUNK_SIZE,
-          :order  => 'id ASC'
-        )
-        break if nodes == []
-        puts "========== migrating nodes #{(page * CHUNK_SIZE) + 1} to #{[((page+1) * CHUNK_SIZE), node_count].min}"
-        nodes.each do |n|
-          n.versions.each do |v|
-            v.migrate!
-          end
-        end
-        page += 1
-      end
-
-      # ============================================ nodes
-      # 1. Set skin_id from skin name
-      NodeMig.find(:all,
-        :conditions => ['site_id = ? AND type = ?', site.id, 'Skin']
-      ).each do |skin|
-        execute "UPDATE nodes SET skin_id = #{skin.id} WHERE skin = '#{skin.name}'"
-      end
-
-      # 2. Set _id from name
-      execute "UPDATE nodes SET _id = name WHERE site_id = #{site.id}"
-    end # sites.each
+      site.migrate!
+    end
 
     # ============================================ roles
     # make properties from dyn_keys list ?
 
     execute "UPDATE roles SET type = 'VirtualClass'"
 
-    # ============================================ site_attributes
-    # migrate to properties
 
     puts "\n\n======== Migration succeded.\n****************** You need to rebuild vhash, fullpath and index now! *************\n=> rake zena:rebuild_vhash && rake zena:rebuild_fullpath && rake zena:rebuild_index\n\n"
   end
