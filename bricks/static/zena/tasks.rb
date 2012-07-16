e8ae4b1d632ff0a909a83481358459f6a9bcf585
Gaspard Bucher <gaspard@teti.ch>
2012-07-16 13:21:17 +0200

    Added static:index rake task

---
+++ bricks/static/zena/tasks.rb
@@ -0,0 +1,26 @@
+
+namespace :static do
+  desc 'rebuild static index'
+  task :index => :environment do
+    # Make sure all bricks are loaded before executing the index rebuild
+    Zena::Use.upgrade_class('Site')
+  
+    include Zena::Acts::Secure
+    if ENV['HOST']
+      sites = [Site.find_by_host(ENV['HOST'])]
+    else
+      sites = Site.all
+    end
+    
+    sites.each do |site|
+      Thread.current[:visitor] = site.any_admin
+      if ENV['WORKER'] == 'false' || RAILS_ENV == 'test'
+        # We avoid SiteWorker.
+        site.rebuild_static_index
+      else
+        # We try to use the site worker.
+        Zena::SiteWorker.perform(site, :rebuild_static_index, nil)
+      end
+    end
+  end
+end
