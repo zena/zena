5f711388e782a8529778cd175678b0febbc02cd3
Gaspard Bucher <gaspard@teti.ch>
2013-08-16 16:49:30 +0200

    Adding support for wkhtmltopdf as pdf rendering engine.

---
+++ bricks/pdf/lib/bricks/pdf/engine/wkhtmltopdf.rb
@@ -0,0 +1,43 @@
+module Bricks
+  module Pdf
+    module Engine
+      module Wkhtmltopdf
+        CMD = Bricks::CONFIG['pdf']['cmd'] || 'wkhtmltopdf'
+        def command(opts)
+          cmd = [CMD]
+          {
+            :http_user     => '--username ',
+            :http_password => '--password ',
+            # No base url option in version 0.9.6.
+            # Put a <base href="www.host.com/"/> at the beginning of the page
+            #:baseurl       => '--baseurl='
+          }.each do |key, cmd_key|
+            cmd << "#{cmd_key}#{opts[key]}"
+          end
+
+          if file = opts[:input]
+            cmd << file
+          else
+            cmd << "-"
+          end
+
+          if file = opts[:output]
+            cmd << file
+          else
+            cmd << "-o -"
+          end
+
+          # FIXME: find a way to redirect errors on screen instead of main log.
+          # Errors redirected to server log file.
+          cmd << "2> #{Zena.log_path}"
+
+          cmd.join(' ')
+        end
+      end # Prince
+    end # Engine
+  end # Pdf
+end # Bricks
+
+
+
+
--- bricks/pdf/lib/bricks/pdf/engine/xhtml2pdf.rb
+++
@@ -1,9 +0,0 @@
-module Bricks
-  module Pdf
-    module Engine
-      module Xhtml2pdf
-        # Not supported until they have http auth and base url settings.
-      end # Xhtml2pdf
-    end # Engine
-  end # Pdf
-end # Bricks
