016041d634f5192f4555341a282ccb3daf5a161c
renoke <renaud.kern@gmail.com>
2016-11-17 09:22:09 +0100

    remove strict sql mode in myslq 5.7

--- config/initializers/abstract_mysql_adapter.rb
+++ config/initializers/abstract_mysql_adapter.rb
@@ -1,3 +1,8 @@
 class ActiveRecord::ConnectionAdapters::MysqlAdapter
   NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+  alias :connect_no_sql_mode :connect
+  def connect
+    connect_no_sql_mode
+    execute("SET sql_mode = ''")
+  end
 end
