90808882a1cd27da05cef1d0246357e5ba3355ad
renoke <renaud.kern@gmail.com>
2016-11-18 08:56:25 +0100

    JSON.load instead of JSON.parse

--- test/unit/string_hash_test.rb
+++ test/unit/string_hash_test.rb
@@ -10,17 +10,17 @@ class StringHashTest < Test::Unit::TestCase
     should 'transform all keys to string' do
       assert_equal Hash['a' => '1', 'b' => '2'], subject
     end
-    
+
     should 'convert to json' do
       assert_match %r{"json_class":"StringHash","data":\{"b":"2","a":"1"|"a":"1","b":"2"\}|"data":\{"b":"2","a":"1"|"a":"1","b":"2"\},"json_class":"StringHash"}, subject.to_json
     end
-    
+
     should 'create from json' do
-      obj = JSON.parse(subject.to_json)
+      obj = JSON.load(subject.to_json)
       assert_equal StringHash, obj.class
       assert_equal obj, subject
     end
-    
+
     should 'merge' do
       subject.merge!(:a => 4, 'c' => '3')
       assert_equal StringHash, subject.class
