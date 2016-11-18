43ccdc18614aa3b2687eabf9e044c072830cf2c1
renoke <renaud.kern@gmail.com>
2016-11-18 08:55:03 +0100

    config availabe locales and monkey patch Gem module

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,26 @@ RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+module Gem
+  def self.source_index
+    sources
+  end
+
+  def self.cache
+    sources
+  end
+
+  SourceIndex = Specification
+
+  class SourceList
+    # If you want vendor gems, this is where to start writing code.
+    def search(*args); []; end
+    def each(&block); end
+    include Enumerable
+  end
+end
+
+
 Rails::Initializer.run do |config|
 
   # We need to explicitly require tzinfo or we get weird erro.
@@ -15,4 +35,6 @@ Rails::Initializer.run do |config|
     :secret => 'jkfawe0[y9wrohifashaksfi934jas09455ohifnksdklh'
   }
   config.action_controller.session_store = :active_record_store
+
+  config.i18n.available_locales = [:fr, :de, :en, :it]
 end
